877c526306618b29bfd7b33e741760411916bf63
minfia <minfialab@gmail.com>
2024-12-10 15:34:28 +0900

    feat: Windowsのシステム設定を追加
    - 国と地域
    - キーボード設定

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,10 +1,16 @@
 <#
     .SYNOPSIS
-        繝｡繧､繝ｳ蜃ｦ逅�
+        メイン処理
 #>
 function Main
 {
+    # Windowsの設定処理
+    . .\sys_config.ps1
+
+    # インストール処理
     . .\install.ps1
+
+    Restart-Computer
 }
 
 Main
